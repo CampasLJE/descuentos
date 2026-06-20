bd1a8eb28c5b4a89acf4abf574cb961787ecd99d
Jesus Campas <al25220289@utcj.edu.mx>
2026-06-20 13:52:02 -0600

    fix: cambios para generar un error en el test

--- tests/test_calculo_descuento.py
+++ tests/test_calculo_descuento.py
@@ -4,7 +4,7 @@ from calc_descuento import descuento
 
 # caso normal con descuentos bajos
 def test_descuentos_correcto():
-    assert descuento(100, 20) == 80.0
+    assert descuento(100, 20) == 99.9 #cambio realizado se cambio de 80 a 99 para que el descuento no sea el correcto
     assert descuento(250, 10) == 225.0
 
 # casos de limites
